d1120942e66dbe1c36a3ffc060f91fd7760b1cc8
Escarleto <lucascliberato31@gmail.com>
2026-03-09 14:59:34 +0000

    Lesson 3.1 - Jump Force

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,8 +4,15 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject ObstaclePrefab;
     private Vector3 SpawnPos = new Vector3(25f, 0, 0);
+    [SerializeField] private float StartDelay = 2f;
+    [SerializeField] private float SpawnInterval = 2f;
 
     private void Start()
+    {
+        InvokeRepeating(nameof(SpawnObstacle), StartDelay, SpawnInterval);
+    }
+    
+    private void SpawnObstacle()
     {
         Instantiate(ObstaclePrefab, SpawnPos, ObstaclePrefab.transform.rotation);
     }
